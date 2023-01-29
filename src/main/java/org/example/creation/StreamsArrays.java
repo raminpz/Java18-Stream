06254d5de7bf2af9f563847ca6a976b62c179ab5
raminpz <raminp.1012@gmail.com>
2023-01-28 22:41:43 -0500

    Initial commit

---
+++ src/main/java/org/example/creation/StreamsArrays.java
@@ -0,0 +1,2 @@
+package org.example.creation;public class StreamsArrays {
+}
---
+++ src/main/java/org/example/creation/StreamsGenerate.java
@@ -0,0 +1,7 @@
+package org.example.creation;
+
+public class InfinitiStreams {
+    public static void main(String[] args) {
+        
+    }
+}
